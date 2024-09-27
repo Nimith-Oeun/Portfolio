ed8686cf08ea155f63b6c76a94bc3faad5e8665d
Nimith Oeun <oeunnimith@gmail.com>
2024-09-27 21:06:46 +0700

    update img1

--- src/components/aboutMe/components/ProfileImg.jsx
+++ src/components/aboutMe/components/ProfileImg.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ProfilePhoto from '../../../assets/profilePhoto.jpg';
+import img1 from '../../../assets/img1.jpg';
 
 const ProfileImage = () => {
   return (
     <div className="flex flex-col w-[37%] max-md:ml-0 max-md:w-full">
-      <img src={ProfilePhoto} alt='Profile Image' className="flex shrink-0 mx-auto max-w-full bg-zinc-300 h-[303px] w-[346px] max-md:mt-10 object-cover" />
+      <img src={img1} alt='Profile Image' className="flex shrink-0 mx-auto max-w-full bg-zinc-300 h-[303px] w-[346px] max-md:mt-10 object-cover" />
     </div>
   );
 };
